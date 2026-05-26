421b739a5a927ff04c154b8025d770ed58623966
DevJose18 <josesielo1805@gmail.com>
2026-05-26 14:07:15 -0600

    Feat: Added Bounce effect

--- RallySport/Assets/_Project/_Scripts/Collisions/DestructibleWall.cs
+++ RallySport/Assets/_Project/_Scripts/Collisions/DestructibleWall.cs
@@ -1,23 +1,57 @@
+using System.Collections;
 using UnityEngine;
 
 public class DestructibleWall : MonoBehaviour
 {
     [SerializeField] private GameObject destructionVFXPrefab;
+    [SerializeField] private GameObject bounceVFXPrefab;
     private ParticleSystem destructionVFX;
-    public void BreakWall(Vector3 pos)
+    private GameObject bounceVFX;
+    public void BreakWall(Vector3 pos, Vector3 normalPos)
     {
         Debug.Log("Wall Destroyed");
-        
+        StartCoroutine(BreakWallCorutine(pos, normalPos));
+    }
+
+    private IEnumerator BreakWallCorutine(Vector3 pos, Vector3 normalPos)
+    {
         //Statement to handle particles instantiation
         if (destructionVFX == null)
         {
-            destructionVFX = Instantiate(destructionVFXPrefab, pos, Quaternion.identity).GetComponent<ParticleSystem>();
+            destructionVFX = Instantiate(destructionVFXPrefab, pos, Quaternion.LookRotation(normalPos)).GetComponent<ParticleSystem>();
+        }
+        gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        if (bounceVFX != null)
+        {
+            Destroy(bounceVFX);
+        }
+        Debug.Log(destructionVFX.main.duration);
+        yield return new WaitForSeconds(destructionVFX.main.duration);
+        Destroy(destructionVFX.gameObject);
+    }
+    
+    public void BounceWall(Vector3 pos, Vector3 normalPos)
+    {
+        StartCoroutine(BounceWallCorutine(pos, normalPos));
+    }
+    
+    private IEnumerator BounceWallCorutine(Vector3 pos, Vector3 normalPos)
+    {
+        Debug.Log("Wall Bounced");
+        if (bounceVFX == null)
+        {
+            bounceVFX = Instantiate(bounceVFXPrefab, pos, Quaternion.LookRotation(normalPos));
+            yield return new WaitForSeconds(0.5f);
+            bounceVFX.SetActive(false);
         }
         else
         {
-            destructionVFX.transform.position = pos;
-            destructionVFX.Play();
+            bounceVFX.SetActive(true);
+            bounceVFX.transform.position = pos;
+            bounceVFX.transform.rotation = Quaternion.LookRotation(normalPos);
+            yield return new WaitForSeconds(0.5f);
+            bounceVFX.SetActive(false);
         }
-        Destroy(gameObject);
     }
 }
