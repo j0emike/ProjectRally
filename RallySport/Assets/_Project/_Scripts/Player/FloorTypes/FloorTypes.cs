8b5c151a30c16fcb10191830c46047bb0044e2e0
Gonzalo Alexander Robles Muñoz <Garm0607@gmail.com>
2026-05-29 23:11:56 -0600

    map

--- RallySport/Assets/_Project/_Scripts/Player/FloorTypes/FloorTypes.cs
+++ RallySport/Assets/_Project/_Scripts/Player/FloorTypes/FloorTypes.cs
@@ -5,9 +5,9 @@ public class FloorTypes : MonoBehaviour
     private KartController kartController;
 
     [Header("Detection Settings")]
-    [SerializeField] private Transform detectionPoint; // Colócalo muy cerca del suelo, entre las llantas traseras o en el centro del kart
-    [SerializeField] private float detectionRadius = 0.6f; // El tamaño del "área circular" de detección
-    [SerializeField] private LayerMask surfaceLayers; // Asegúrate de marcar Basic, Mud, Ice y Sand aquí
+    [SerializeField] private Transform detectionPoint;
+    [SerializeField] private float detectionRadius = 0.6f;
+    [SerializeField] private LayerMask surfaceLayers;
 
     [System.Serializable]
     public struct SurfacePreset
@@ -41,30 +41,25 @@ public class FloorTypes : MonoBehaviour
 
     private void DetectSurface()
     {
-        // Guardará todos los colisionadores que entren en la esfera de detección
         Collider[] hitColliders = Physics.OverlapSphere(detectionPoint.position, detectionRadius, surfaceLayers);
 
         if (hitColliders.Length > 0)
         {
-            // Creamos variables para saber qué capas encontramos dentro de la esfera
             bool foundSpecialSurface = false;
             string chosenLayer = "Basic";
 
-            // Revisamos TODOS los colisionadores que están tocando las llantas/kart
             foreach (Collider col in hitColliders)
             {
                 string layerName = LayerMask.LayerToName(col.gameObject.layer);
 
-                // Si encontramos una capa especial, la priorizamos de inmediato y salimos del bucle
                 if (layerName == "Mud" || layerName == "Ice" || layerName == "Sand")
                 {
                     chosenLayer = layerName;
                     foundSpecialSurface = true;
-                    break; // Rompe el ciclo porque ya encontramos la superficie importante
+                    break;
                 }
             }
 
-            // Aplicamos los valores de la capa elegida (si no encontró especial, por defecto será "Basic")
             switch (chosenLayer)
             {
                 case "Basic":
@@ -86,7 +81,6 @@ public class FloorTypes : MonoBehaviour
         }
         else
         {
-            // Si el kart vuela o sale de los colisionadores
             ApplySurfaceSettings(basicSurface);
         }
     }
@@ -98,7 +92,6 @@ public class FloorTypes : MonoBehaviour
         kartController.Deceleration = preset.deceleration;
     }
 
-    // Para ver la esfera en la pestaña de Scene y ajustarla perfectamente
     private void OnDrawGizmosSelected()
     {
         if (detectionPoint == null) return;
