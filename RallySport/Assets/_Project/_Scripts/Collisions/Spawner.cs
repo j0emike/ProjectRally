655dc2280b9989ba9f4d187e0304243d03bced77
Zarek Saleme <jorge.saleme77@gmail.com>
2026-06-06 15:29:40 -0600

    feat: implementacion de respawn, checkpoints y deteccion offroad

--- RallySport/Assets/_Project/_Scripts/Collisions/Spawner.cs
+++ RallySport/Assets/_Project/_Scripts/Collisions/Spawner.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _carPrefab;
-    [SerializeField] private Transform _spawnPoint;
     
     public static Spawner LastSpawn;
 
-    public Vector3 SpawnPosition => _spawnPoint != null ? _spawnPoint.position : transform.position;
-    public Quaternion SpawnRotation => _spawnPoint != null ? _spawnPoint.rotation : transform.rotation;
+    public Vector3 PlayerEnterPosition { get; private set; }
+    public Quaternion PlayerEnterRotation { get; private set; }
 
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.CompareTag("Player"))
         {
             LastSpawn = this;
+            PlayerEnterPosition = collider.transform.position;
+            PlayerEnterRotation = collider.transform.rotation;
             Debug.Log("Car Entered Spawn");         
         }
     }
